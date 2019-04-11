e889ea02b2e929070bf6709bce9153cab99448ef
Mario <mariomatamosquitos@hotmail.es>
2019-04-12 00:19:26 +0200

    675

--- src/Principal.java
+++ src/Principal.java
@@ -3,11 +3,11 @@ public class Principal {
     Arbol arbol = new Arbol();
     Alumno alumno = new Alumno();
         Alumno alumno8 = new Alumno("","",1);
-        Alumno alumno1 = new Alumno("","",30);
+        Alumno alumno1 = new Alumno("a","",10);
         Alumno alumno2 = new Alumno("","",4);
         Alumno alumno3 = new Alumno("","",6);
         Alumno alumno4 = new Alumno("","",8);
-        Alumno alumno5 = new Alumno("","",10);
+        Alumno alumno5 = new Alumno("b","",10);
         Alumno alumno6 = new Alumno("","",2);
         Alumno alumno7 = new Alumno("","",1);
         arbol.insertar(7,alumno);arbol.insertar(2,alumno1); arbol.insertar(3,alumno2);arbol.insertar(4,alumno3);
